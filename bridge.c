f4c720575b68dca09d1992cfe57ca0da18f2156a
bigstar9906 <52996753+bigstar9906@users.noreply.github.com>
2021-07-13 23:44:44 +0900

    Update bridge.c
    combine 리턴하면 된다는 거 확인

--- bridge.c
+++ bridge.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int addall(int);
+int addall(int,int);
 
 int main(void)
 {
@@ -29,13 +29,8 @@ int main(void)
     //바로 result 값이 들어가 result[n] 의 역할을 수행.
     for(int i=0;i<n;i++)
     {
-        int temp=Marr[i]-Narr[i]+1;
-        int result=0;
-        for(int j=1;j<temp+1;j++)
-        {
-            result += addall(j);
-        }
-        Marr[i]=result;
+        
+        //combine return
     }
     for(int i=0;i<n;i++)
     {
@@ -47,44 +42,8 @@ int main(void)
 }
 
 
-int addall(int x)
+int Combine(int x,int y)
 {
-    return (x+1)*x*0.5;
+    int temp;
+    return 0;
 }
-
-/*
-
-지금 개수 세는 방식이 잘못됬음 다시 수정필요.
-1 4
-2 3
-
-12-54321
-23-4321
-34-321
-45-21
-56-1
-ㄱ 1
-ㄴ 2
-ㄷ 3 
-ㄹ 
-123 - 4
-124 - 3
-125 - 2
-126 - 1
-134 - 3
-135 - 2
-136 - 1
-145 -2
-146 -1
-156 -1
-234 - 3
-235 - 2
-236 - 1
-245 -2
-246 -1
-256 -1
-345 -2
-346 -1
-356 -1
-456 -1
-*/
