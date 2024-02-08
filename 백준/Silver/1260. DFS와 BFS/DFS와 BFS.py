32a4791100395aa57f2fd5ae502ad3a4b1ba75bf
bigstar9906 <52996753+bigstar9906@users.noreply.github.com>
2024-02-08 11:07:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 612 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,41 @@
+n,m,v = map(int,input().split())
+node = []
+for i in range(m):
+  node.append(list(map(int,input().split())))
+node.sort(key=lambda x: (x[0]+x[1]))
+visited = [False] * (n+1)
+def dfs(node, v, visited):
+  visited[v] = True
+  print(v,end=' ')
+  queue = []
+  for i in range(0,m):
+    if node[i][0]==v and not visited[node[i][1]]:
+      queue.append(node[i][1])
+    elif node[i][1]==v and not visited[node[i][0]]:
+      queue.append(node[i][0])
+    if queue:
+      queue.sort()
+      for i in queue:
+        if not visited[i]:
+          dfs(node,i,visited)
+      
+  return
+
+def bfs(node, v, visited):
+  queue = [v]
+  visited[v] = True
+  while queue:
+    v = queue.pop(0)
+    print(v,end=' ')
+    for i in range(0,m):
+      if node[i][0]==v and not visited[node[i][1]]:
+        queue.append(node[i][1])
+        visited[node[i][1]] = True
+      elif node[i][1]==v and not visited[node[i][0]]:
+        queue.append(node[i][0])
+        visited[node[i][0]] = True
+  return
+dfs(node,v,visited)
+visited = [False] * (n+1)
+print()
+bfs(node,v,visited)
