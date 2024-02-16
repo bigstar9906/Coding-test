2dd56689e43c81868194949e4bf71a900efd6fdd
bigstar9906 <52996753+bigstar9906@users.noreply.github.com>
2024-02-16 15:13:20 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 160.07 ms, Memory: 18.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+
+def bfs(land,i,j,minmax):
+    if i < 0 or j<0 or i>len(land)-1 or j>len(land[0])-1:
+        return 0
+    elif land[i][j] ==0:
+        return 0
+    else :
+        land[i][j] = 0
+        minmax[0] = min(minmax[0],j)
+        minmax[1] = max(minmax[1],j)
+        return bfs(land,i+1,j,minmax)+ bfs(land,i-1,j,minmax)+bfs(land,i,j+1,minmax)+bfs(land,i,j-1,minmax)+1
+
+def solution(land):
+    answer = 0
+    n = len(land)
+    m = len(land[0])
+    answer = [0]*len(land[0])
+    for col in range (0,m):
+        for row in range(0,n):
+            minmax = [col,col]
+            value = bfs(land,row,col,minmax)
+            if value!=0:
+                for x in range(minmax[0],minmax[1]+1):
+                    answer[x]+=value        
+    return max(answer)
