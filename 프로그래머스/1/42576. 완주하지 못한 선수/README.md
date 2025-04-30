7c739d30362c7b34cbbb2adbe9199a70eb7ac7f8
TAEGYU IM <52996753+bigstar9906@users.noreply.github.com>
2025-05-01 03:07:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.67 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 38.8 MB, 시간: 30.70 ms
+메모리: 36 MB, 시간: 86.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 11:36:17
+2025년 05월 01일 03:07:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,9 @@
 from collections import Counter
 def solution(participant, completion):
-    answer = []
-    complete = Counter(completion)
-    runners = Counter(participant)
-    for runner in participant:
-        if runner not in complete :
-            return runner
-        elif runners[runner]!=complete[runner]:
-            return runner
-    
+    answer = ''
+    cnt = Counter()
+    for p in participant :
+        cnt[p]+=1
+    for c in completion:
+        cnt[c]-=1
+    return cnt.most_common()[0][0]
