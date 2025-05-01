1ff25b2f9c50b28cc8a36b8745da4272021e5e91
TAEGYU IM <52996753+bigstar9906@users.noreply.github.com>
2025-05-01 22:54:07 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,12 @@
 import math
 def solution(progresses, speeds):
     answer = []
-    prior = 0
-    task = 0
-    for idx in range(len(progresses)):
-        current = math.ceil((100-progresses[idx])/speeds[idx])
-        if prior ==0:
-            prior = current
-            task = 1
-        elif current>prior:
-            answer.append(task)
-            task = 1
-            prior = current
-        else :
-            task+=1
-    answer.append(task)
+    temp = [math.ceil((100-progresses[0])/speeds[0]),0]
+    for i in range(1,len(progresses)):
+        current = math.ceil((100-progresses[i])/speeds[i])
+        if temp[0]<current:
+            answer.append(i-temp[1])
+            temp=[current,i]
+    answer.append(len(progresses)-temp[1])
         
     return answer
