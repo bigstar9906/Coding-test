534220ab758973ccad0f38c2ff3393ab7ebdf151
TAEGYU IM <52996753+bigstar9906@users.noreply.github.com>
2025-05-01 18:47:23 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,9 @@
 from collections import Counter
 def solution(clothes):
+    cnt = Counter()
     answer = 1
-    cloth_count = Counter()
-    for cloth in clothes:
-        cloth_count[cloth[1]]+=1
-    for sortOf in cloth_count.keys():
-        answer*=cloth_count[sortOf]+1
-    
+    for c in clothes:
+        cnt[c[1]]+=1
+    for case in cnt:
+        answer*=(cnt[case]+1)
     return answer-1
