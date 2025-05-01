e57c9e6f2101b112c87218e1e8ddfdd2469e6686
TAEGYU IM <52996753+bigstar9906@users.noreply.github.com>
2025-05-02 00:04:28 +0900

    [level 2] Title: 올바른 괄호, Time: 17.38 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,11 +1,8 @@
 def solution(s):
     answer = True
     open_cnt = 0
-    for x in s:
-        if x =='(':
-            open_cnt+=1
-        elif open_cnt>0:
-            open_cnt-=1
-        else:
+    for l in s:
+        open_cnt += 1 if l=="(" else -1
+        if open_cnt<0:
             return False
     return open_cnt==0
