a5ad591e241e6a42e96bc4d460b5a9a157fd9178
TAEGYU IM <52996753+bigstar9906@users.noreply.github.com>
2025-05-02 19:55:23 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.38 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,16 +1,22 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    on = deque()
-    on_time = deque()
-    while len(truck_weights)>0:
-        if sum(on)+truck_weights[0]<=weight and len(on)+1<=bridge_length:
-            on.append(truck_weights[0])
-            on_time.append(answer)
+    onBridge = 0
+    onBridge_arr=deque()
+    for truck in truck_weights:
+        if onBridge+truck>weight:
+            while onBridge+truck>weight:
+                out = onBridge_arr.pop()
+                onBridge-=out[0]
+                answer = out[1]+bridge_length
+            onBridge_arr.appendleft([truck,answer])
+            onBridge+=truck
+            if onBridge_arr[-1][1]+bridge_length==answer+1:
+                onBridge-=onBridge_arr.pop()[0]
+        else:
             answer+=1
-            del truck_weights[0]
-        else :
-            out = on.popleft()
-            out_time = on_time.popleft()
-            answer = max(answer,out_time+bridge_length)
-    return on_time[-1]+1+bridge_length
+            onBridge+=truck
+            onBridge_arr.appendleft([truck,answer])
+            if onBridge_arr[-1][1]+bridge_length==answer+1:
+                onBridge-=onBridge_arr.pop()[0]
+    return answer+bridge_length
